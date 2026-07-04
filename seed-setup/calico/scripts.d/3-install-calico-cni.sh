f33f66391562755ad2c70bf6fe19f1faf8f34b10
Jens Wilke <jw_github@headissue.com>
2026-07-04 09:45:00 +0600

    Enable encryption for node to node internal traffic

--- seed-setup/calico/scripts.d/3-install-calico-cni.sh
+++ seed-setup/calico/scripts.d/3-install-calico-cni.sh
@@ -10,3 +10,29 @@ curl -sS -O https://raw.githubusercontent.com/projectcalico/calico/v3.31.3/manif
 sed -i 's|quay.io/|docker.io/|g' calico.yaml
 
 kubectl apply -f calico.yaml
+
+# wait for CRD
+kubectl wait --for condition=Established crd/felixconfigurations.crd.projectcalico.org --timeout=60s
+
+#until kubectl api-resources | grep -q '^felixconfigurations'; do
+#  echo "waiting for FelixConfiguration API..."
+#  sleep 2
+#done
+
+
+# Switch on wireguard encryption
+# Its a common pattern that internal cluster traffic is unencrypted (http instead of https)
+# With this setting we encrypt all internal traffic leaving a node
+
+cat >> calico-wireguard.yaml <<'EOF'
+---
+apiVersion: crd.projectcalico.org/v1
+kind: FelixConfiguration
+metadata:
+  name: default
+spec:
+  wireguardEnabled: true
+  wireguardEnabledV6: true
+EOF
+
+kubectl apply -f calico-wireguard.yaml
