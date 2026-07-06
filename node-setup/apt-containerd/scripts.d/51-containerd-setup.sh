79ababe2f56ae7871ab6044702fb61febd8ebf5f
Jens Wilke <jw_github@headissue.com>
2026-07-06 18:53:36 +0600

    Clean proxy config / working configuration with Zot

--- node-setup/apt-containerd/scripts.d/51-containerd-setup.sh
+++ node-setup/apt-containerd/scripts.d/51-containerd-setup.sh
@@ -3,47 +3,58 @@ set -e
 
 # Registry proxy
 #
-# https://registry.internal:5000 docker.io mirror
-# https://registry.internal:5001 registry.k8s.io mirror
-mirror=https://registry.internal:5001
+registries=`cat - <<EOF
+https://registry.internal:5000 docker.io
+https://registry.internal:5001 registry.k8s.io
+https://registry.internal:5002 quay.io
+https://registry.internal:5003 public.ecr.aws
+https://registry.internal:5004 codeberg.org
+https://registry.internal:5005 ghcr.io
+EOF`
 
+mirror=https://registry.internal:5001
 if curl -sS -o /dev/null --max-time 3 "$mirror"/v2/ 2>/dev/null; then
 
-hosts=/etc/containerd/certs.d/docker.io/hosts.toml
-mkdir -p $(dirname "$hosts")
-cat - > $hosts <<EOF
-# if this is not specified it would be docker.io
-server = "https://registry.internal:5000"
+echo "$registries" | while read server registry; do
+  hosts=/etc/containerd/certs.d/$registry/hosts.toml
+  mkdir -p $(dirname "$hosts")
+  cat - > $hosts <<EOF
+# configured by cluster-lab 51-containerd-setup.sh
 
-[host."https://registry.internal:5000"]
+# if this is not specified it would be docker.io
+server = "$server"
+
+[host."$server"]
   capabilities = ["pull", "resolve", "push"]
 EOF
-
-hosts=/etc/containerd/certs.d/registry.k8s.io/hosts.toml
-mkdir -p $(dirname "$hosts")
-cat - > $hosts <<EOF
-# if this is not specified it would be docker.io
-server = "https://registry.internal:5001"
-
-[host."https://registry.internal:5001"]
-  capabilities = ["pull", "resolve", "push"]
-EOF
-
-echo Mirror configured: https://registry.internal:5000 -> docker.io and https://registry.internal:5001 > registry.k8s.io
+echo "OCI mirror for $registry -> $server"
+done
 
 elif curl -sS -o /dev/null --max-time 3 https://zotregistry.internal:5000/v2/ 2>/dev/null; then
+
+server="https://zotregistry.internal:5000"
+
 # proxy setup for zot
-for R in docker.io k8s.io; do
+echo "$registries" | while read IGNORE R; do
 hosts=/etc/containerd/certs.d/$R/hosts.toml
 mkdir -p $(dirname "$hosts")
 cat - > $hosts <<EOF
-# if this is not specified it would be docker.io
-server = "https://zotregistry.internal:5000/v2/$R"
+# configured by cluster-lab 51-containerd-setup.sh
 
-[host."https://zotregistry.internal:5000"]
-  capabilities = ["pull", "resolve", "push"]
-  override_path = true
+# if the proxy registry is not available this will be  a hard fail, no fallback to the origin
+# Zot does not support "?ns=", so we need to send the registry within the path
+server = "$server/v2/$R"
+capabilities = ["pull", "resolve", "push"]
+override_path = true
+
+# containerd first requests the host entries and then falls back 
+# to the server setting above.
+# [host."$server/v2/$R"]
+#  capabilities = ["pull", "resolve", "push"]
+#  override_path = true
 EOF
+echo "OCI mirror for $R -> $server"
+
 done
 else
   echo "No image mirror detected"
@@ -85,7 +96,10 @@ exit 0
 
 # testing
 
+# ctr does not use containerd config:
 ctr image pull docker.io/library/alpine:latest
 ctr image pull --hosts-dir /etc/containerd/certs.d -- docker.io/library/alpine:latest
+
+kubeadm config images pull --kubernetes-version "v${KUBE_VERSION#v}"
 crictl pull docker.io/library/alpine:latest
 
